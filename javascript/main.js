cb01fe520bc9ce6eb4258b7d43e3288f039f2767
Svinci131 <svinci131@gmail.com>
2016-01-31 12:35:52 -0500

    backing up

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,20 @@
 var fs = require('fs');
 
-fs.writeFile('helloworld.txt', 'Hello World!', function (err) {
-  if (err) return console.log(err);
-  console.log('Hello World > helloworld.txt');
+// fs.writeFile('helloworld.txt', 'Hello World!', function (err) {
+//   if (err) return console.log(err);
+//   console.log('Hello World > helloworld.txt');
+// });
+
+var http = require('http');
+
+
+var server = http.createServer(function(request,response){
+	 response.writeHead(200, {"Content-Type": "text/json"});
+	 fs.readFile('data.json', 'utf8', function(err,data){
+	 	console.log(data)
+	 	response.write( data );
+	 	response.end();
+	 })
 });
+
+server.listen(8080);
--- javascript/scrapper.js
+++ javascript/scrapper.js
@@ -1,53 +1,170 @@
 var request = require("request"),
     cheerio = require("cheerio"),
-    fs = require('fs'),
-    obj = {};
+    fs      = require('fs'),
+    Q       = require('q'),
+    obj     = {};
 
-var url= "http://mylanguages.org/multimedia/bengali_audio_colors.php";
-var group = url.split("_");
+var url   = "http://mylanguages.org/learn_bengali.php";
+//var test  = "http://mylanguages.org/multimedia/bengali_audio_colors.php";
+
+
+var requestPromise = function( url ) {
+  var d = Q.defer();
+
+  request( url, function( err, response, body ){
+    if ( err ) {
+      d.reject( err );
+    }
+    else {
+      d.resolve( body );
+    }
+  });
+
+  return d.promise;
+}
+
+console.log( 'Fetching: ' + url + '\n' );
+requestPromise( url )
+.then(function(body){
+  var $ = cheerio.load(body),
+      arr = [],
+      links = $(".sidebar-menu li a").each(function(idx, el) {
+      el = $(el);
+      if (idx >= 8 && idx<=33){
+        // popObj( el.attr('href'), idx, 32 );
+        console.log( 'About to fetch: ' + el.attr('href') + '\n' );
+        arr.push( fetchData( el.attr('href') ) );
+      }
+  });
+
+  return Q.all( arr );
+})
+.then(function(){
+  console.log( 'writing to file...' );
+  fs.writeFile('data.json', JSON.stringify(obj), function( err ) {
+    console.log('done!');
+  });
+})
+.fail(function(err){
+
+});
+
+function fetchData( url ) {
+  var d = Q.defer();
+
+  requestPromise( url ).then(function(body) {
+    console.log( 'Found: ' + url + '\n' );
+    var group = url.split("_");
     group = group[group.length-1].split(".")[0]
+    var $ = cheerio.load(body),
 
+        word = $(".table tbody").eq(1).children().each(function (idx, el) { 
+                  el = $(el);
+                  var english = el.find('img').attr('alt');
+                  // console.log( english ) 
+                  var bLetters = el.find('b').html();
+                  var all = el.find('td').text();
+                  if ( !english || !english.length ) {
+                    return;
+                  }
+                  var allEIndx = english.length; 
+                  var bangla = all.substring (allEIndx)
+                  var bLetterIndex = null;
 
+                  for (var i = 0; i < bangla.length; i++){
+                    if (bangla.charCodeAt(i) > 123){
+                      var bLetterIndex = i; 
+                      break;
+                    }
+                  }
+                  bangla = bangla.substring (0, bLetterIndex)
 
+                  if ( typeof obj[group] === "undefined") {
+                    obj[group] = {};
+                  }
+
+                  obj[group][english] = { 
+                    "english": english,
+                    "type": group, 
+                    "bangla": bangla,
+                    "bLetters": bLetters,
+                    "index":idx
+                  };
+      });
+      d.resolve();
+  });
+
+  return d.promise;
+}
+
+/*
 request(url, function (error, response, body) {
     if (!error) {
       var $ = cheerio.load(body),
-
-      word = $(".table tbody").eq(1).children().each(function (idx, el) { 
-                el = $(el);
-                var english = el.find('img').attr('alt'); 
-                var bLetters = el.find('b').html();
-                var all = el.find('td').text();
-                var allEIndx = english.length; 
-                var bangla = all.substring (allEIndx)
-                var bLetterIndex = null;
-
-                for (var i = 0; i < bangla.length; i++){
-                  if (bangla.charCodeAt(i) > 123){
-                    var bLetterIndex = i; 
-                    break;
-                  }
-                }
-                bangla = bangla.substring (0, bLetterIndex)
-
-                if ( typeof obj[group] === "undefined") {
-                  obj[group] = {};
-                }
-
-                obj[group][english] = { 
-                  "english": english,
-                  "type": group, 
-                  "bangla": bangla,
-                  "bLetters": bLetters,
-                  "index":idx
-                };
+      links = $(".sidebar-menu li a").each(function(idx, el) {
+        el = $(el);
+        if (idx >= 8 && idx<=32){
+          // console.log(el.attr("href"), idx);
+          popObj( el.attr('href'), idx, 32 );
+        }
       });
-      
-      console.log(obj)
-    } else {
-      console.log("We’ve encountered an error: " + error);
     }
+});
 
-  });
+function popObj( test, curr, last ) {
+  request(test, function (error, response, body) {
+      if (!error) {
+        var $ = cheerio.load(body),
 
+        word = $(".table tbody").eq(1).children().each(function (idx, el) { 
+                  el = $(el);
+                  var english = el.find('img').attr('alt');
+                  console.log( english, curr, last ) 
+                  var bLetters = el.find('b').html();
+                  var all = el.find('td').text();
+                  if ( !english || !english.length ) {
+                    return;
+                  }
+                  var allEIndx = english.length; 
+                  var bangla = all.substring (allEIndx)
+                  var bLetterIndex = null;
 
+                  for (var i = 0; i < bangla.length; i++){
+                    if (bangla.charCodeAt(i) > 123){
+                      var bLetterIndex = i; 
+                      break;
+                    }
+                  }
+                  bangla = bangla.substring (0, bLetterIndex)
+
+                  if ( typeof obj[group] === "undefined") {
+                    obj[group] = {};
+                  }
+
+                  obj[group][english] = { 
+                    "english": english,
+                    "type": group, 
+                    "bangla": bangla,
+                    "bLetters": bLetters,
+                    "index":idx
+                  };
+
+                  if ( curr === last ) {
+                    console.log( obj );
+                  }
+        });
+        
+        //console.log(obj)
+      } else {
+        //console.log("We’ve encountered an error: " + error);
+      }
+
+    });  
+}
+
+setTimeout(function(){
+  console.log('#######')
+  console.log(obj)
+}, 5000)
+
+*/
