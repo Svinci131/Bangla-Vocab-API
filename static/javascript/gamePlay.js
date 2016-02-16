fda2f8fd5b9003b55ee5c0949a6e6f396eb4d856
Svinci131 <svinci131@gmail.com>
2016-02-15 19:38:35 -0500

    set up, gulp, browserify and react

---
+++ static/javascript/gamePlay.js
@@ -0,0 +1,42 @@
+var imgObj = require('./dataWimagesNEW'); 
+
+console.log(imgObj)
+// // Create buttons for each category 
+// $("#content").prepend("<p>hello</p>")
+
+// $.get('/javascript/dataWimagesNEW.json')
+// .then(function(data){
+// 	console.log( data );
+// })
+
+
+// var Container = React.createClass({
+// 	//state - home page 
+// 	render: function() {
+// 		return (<div>TEST</div>)
+// 	}
+	
+
+// });
+
+
+// React.render(
+//   <Container myDataRef={myDataRef} />,
+//   document.getElementById('content')
+// );
+
+
+// //two pages- home and gamePlay 
+
+// //home 
+// //Create buttons for each category 
+
+
+// //Click a button 
+// //remove buttons
+// //load flascards
+// //load back button 
+
+// //click back button 
+// //go back to home page 
+
---
+++ static/javascript/gulpfile.js
@@ -0,0 +1,35 @@
+// var gulp = require('gulp');
+
+// gulp.task ('hello', function () {
+// 	console.log("hello")
+// })
+
+
+// require the necessary dependencies
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify'); // yes, browserify can 
+// also be called inside a .js file in Node
+var babelify = require('babelify');
+
+// set up tasks
+gulp.task('browserify', function() {
+    // this task will run browserify, just like we did from terminal
+    // but inside this js file instead
+    return browserify('./index.js')
+        .transform( "babelify", {presets: ["react"]})
+        // this .bundle() method walks through the code
+        // and concatenates all the dependencies in order
+        .bundle()
+        // here, we take the concatenated dependencies
+        // and "pipe" it to the bundled.js file
+        .pipe( source( 'bundled.js' ) )
+        // then we write this file to the current directory
+        .pipe( gulp.dest('./') );
+});
+
+// this is the awesome part
+// with "watch", we can tell gulp to "watch" all the files
+// in this directory and IF any of those files changes
+// we instruct it to call our browserify task
+gulp.watch('*', ['browserify']);
