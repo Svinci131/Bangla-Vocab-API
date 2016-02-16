618818538f88f7c38a5fdb125a57a80877f0f358
Svinci131 <svinci131@gmail.com>
2016-02-15 23:08:46 -0500

    set up routers

---
+++ static/javascript/hello.js
@@ -0,0 +1,7 @@
+var React = require('react');
+
+module.exports = React.createClass({
+	render: function() {
+		return (<a href="/#foo/1">Click here</a>);
+	}
+})
--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,59 +1,77 @@
+var Router = require('director').Router;
 var React = require('react');
 var ReactDOM = require('react-dom');
 var imgObj = require('./dataWimagesNEW'); 
 var data = imgObj;
-// var data = Object.keys( imgObj).reduce(function( arr, currentItem) {
-// 	arr.push( imgObj[ currentItem ] );
-// 	return arr;
-// }, []);
+
+var Hello = require('./hello');
+
+var routes = {
+	'/home': function() {
+		ReactDOM.render(
+		  <Hello />,
+		  document.getElementById('container')
+		);
+	},
+	'/foo/:id': function( id ) {
+		console.log( id )
+		ReactDOM.render(
+		  <Foo id={id} />,
+		  document.getElementById('container')
+		);
+	}
+}
+
+console.log( routes, Router);
+
+var router = Router( routes );
+router.init('/home');
+
+var Foo = React.createClass({
+	render: function() {
+		return (<h1>Hello, Wrold! {this.props.id} </h1>)
+	}
+})
 
 
-// console.log( data );
-// <CardPage data={this.props.data}/>
-// var data = [
-//   {id: 1, author: "Pete Hunt", text: "This is one comment"},
-//   {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
-// ];
+// var Hello = React.createClass({
+//   render: function() {
+//     return <div>
+//     <h1>Learn Bangla</h1>
+//     	<HomePage />
+//     </div>;
+//   }
+// });
 
+// var HomePage = React.createClass({
+//   loadCategory: function (event) {
+//   	console.log('foo')
+//   },
+//   render: function() {
+//   	var categories = Object.keys(data).reduce(function( arr, currentItem) {
+// 			arr.push( currentItem);
+// 			return arr;
+// 	}, []);
 
-var Hello = React.createClass({
-  render: function() {
-    return <div>
-    <h1>Learn Bangla</h1>
-    	<HomePage />
-    </div>;
-  }
-});
+//   	var categoryButtons = categories.map(function(title) {
+//   		return (
+// 			<button onClick={function(e) {
+// 				console.log(e.target}
+// 				} key={title}>
+// 	          {title}
+// 	        </button>
+// 	      );
+//   	});
+//     return (
+//      <div> {categoryButtons} </div>
+//     );
+//   }
+// });
 
-var HomePage = React.createClass({
-  loadCategory: function (event) {
-  	console.log('foo')
-  },
-  render: function() {
-  	var categories = Object.keys(data).reduce(function( arr, currentItem) {
-			arr.push( currentItem);
-			return arr;
-	}, []);
-
-  	var categoryButtons = categories.map(function(title) {
-  		return (
-			<button onClick={function(e) {
-				console.log(e.target}
-				} key={title}>
-	          {title}
-	        </button>
-	      );
-  	});
-    return (
-     <div> {categoryButtons} </div>
-    );
-  }
-});
-
-ReactDOM.render(
-  <Hello data={data} />,
-  document.getElementById('container')
-);
+// ReactDOM.render(
+//   <Hello data={data} />,
+//   document.getElementById('container')
+// );
 
 
 
