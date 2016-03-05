78d520e9809c291f7b2365b4b6c05ba377b9feee
Svinci131 <svinci131@gmail.com>
2016-03-04 23:32:26 -0500

    started researching sound/letters, bangla charecters show up always now

--- javascript/audioScrapper.js
+++ javascript/audioScrapper.js
@@ -1,30 +1,26 @@
+//http://www.shabdkosh.com/bn/translate?e=cheese&l=bn
+
+var obj = require('./dataWimages');
+
+//for each cat for each obj 
+
+//http://www.shabdkosh.com/bn/translate?e=WORD&l=bn
+
 var request = require("request"),
   cheerio = require("cheerio"),
   // url="https://en.wikibooks.org/wiki/Bengali/Sounds"
   //http://www.lexilogos.com/keyboard/bengali.htm
   
-url = "https://en.wikipedia.org/wiki/Bengali_alphabet"
+url = "http://www.shabdkosh.com/bn/translate?e=cheese&l=bn"
 
 request(url, function (error, response, body) {
 	console.log("hello")
   if (!error) {
     var $ = cheerio.load(body),
-
-    // table = $("th").each(function (el) {
-    // 	el = $(el)
-    // 	console.log(el)
-    // 	//get the head and each col text by order
-    // });
-   // console.log(table)
     table = $('.wikitable').children().each(function(el){
     	console.log("here", (el))
     })
-    //not the first 
-
-    // word = $(".word-and-pronunciation h1").html();
-    // def = $(".definitions-page .card-box p").html()
-      
-    
+ 
   } else {
     console.log("We’ve encountered an error: " + error);
   }
--- static/javascript/bLetters.js
+++ static/javascript/bLetters.js
@@ -3,7 +3,7 @@ var Entities = require('html-entities').AllHtmlEntities;
 
 module.exports = React.createClass({
 	convertFromHex: function () {
-		console.log( this.props.data )
+		
 		var hexEntities = this.props.data.bLetters; 
 		hexEntities = hexEntities.split(";");
 		hexEntities = hexEntities.slice(0, hexEntities.length-1)
@@ -15,14 +15,28 @@ module.exports = React.createClass({
 		}, []);
 
  		var bLetters = arr.join(" ")
-
+ 		// console.log("here", bLetters)
  		return (bLetters)
 
 	},
+	determineType:function () {
+
+    	var bLetters = this.props.data.bLetters
+    	if (bLetters[0] === '&'){
+    		return (<span>{this.convertFromHex()}</span>)
+    		console.log ("here", bLetters[0])
+    	}
+    	else {
+    		return (<span>{bLetters}</span>)
+    	}
+
+    
+	},
     render: function() {
+
         return (
             <p>
-                {this.convertFromHex()}
+                {this.determineType()}
             </p>
         );
     }
